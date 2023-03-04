bdb567e5a0d61a62e2ada629566f240dd6c08143
Matsuel <matsuel0308@gmail.com>
2023-03-05 00:03:02 +0100

    move top

--- Board.java
+++ Board.java
@@ -3,8 +3,6 @@ import java.util.Random;
 
 public class Board extends Case {
     private ArrayList<ArrayList<Case>> Cases;
-    private final int row=4;
-    private final int col=4;
 
     public Board(){
         this.Cases= new ArrayList<ArrayList<Case>>();
@@ -41,18 +39,79 @@ public class Board extends Case {
         return false;
     }
 
+    public void InitCases(){
+
+        for (int i=0;i<4;i++){
+            ArrayList<Case> c= new ArrayList<Case>();
+            for (int j=0;j<4;j++){
+                Case c1= new Case(i, j, 0);
+                c.add(c1);
+            }
+            this.Cases.add(c);
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> GetEmpty(){
+        ArrayList<ArrayList<Integer>> empty= new ArrayList<ArrayList<Integer>>();
+        for (ArrayList<Case> col: this.GetCases()){
+            for (Case c: col){
+                if (c.GetValue()==0){
+                    ArrayList<Integer> pos= new ArrayList<Integer>();
+                    pos.add(c.GetX());
+                    pos.add(c.GetY());
+                    empty.add(pos);
+                }
+            }
+        }
+        return empty;
+    }
+
     public void Spawn(){
         ArrayList<Integer> v= new ArrayList<Integer>();
         v.add(2);
         v.add(4);
         Random va= new Random();
         int val=va.nextInt(1 - 0 + 1) + 0;
-        Random r = new Random();
-        int row=r.nextInt(3 - 0 + 1) + 0;
-        Random c = new Random();
-        int col=c.nextInt(3 - 0 + 1) + 0;
-        Case ca= new Case(row, col, v.get(val));
-        this.SetCaseAt(row, col, ca);
+        Random r= new Random();
+        Integer pos= r.nextInt(this.GetEmpty().size()-0+1)+0;
+        Case ca = new Case(this.GetEmpty().get(pos).get(0), this.GetEmpty().get(pos).get(1), v.get(val));
+        this.SetCaseAt(this.GetEmpty().get(pos).get(0), this.GetEmpty().get(pos).get(1), ca);
+    }
+
+    public void MoveBottom(){
+
+        for (int i=0; i<3;i++){
+            for (int j=0; j<4;j++){
+                Integer val = this.GetCaseAt(i, j).GetValue();
+                Integer val1 = this.GetCaseAt(i+1, j).GetValue();
+
+                if (val==val1){
+                    Case c = new Case(i+1, j, val+val1);
+                    this.SetCaseAt(i+1, j, c);
+                    Case cnul = new Case(i, j, 0);
+                    this.SetCaseAt(i, j, cnul);
+                }else if (val1==0){
+                    Case c = new Case(i+1, j, val);
+                    this.SetCaseAt(i+1, j, c);
+                    Case cnul = new Case(i, j, 0);
+                    this.SetCaseAt(i, j, cnul);
+                }
+
+            }
+        }
+
+    }
+
+    public void MoveTop(){
+        
+    }
+
+    public void MoveLeft(){
+        
+    }
+
+    public void MoveRight(){
+        
     }
 
     @Override 
--- Test.java
+++ Test.java
@@ -1,60 +1,12 @@
-import java.util.ArrayList;
-
-// import java.util.ArrayList;
-
-// import javax.swing.border.Border;
-
-// public class Test {
-//     public static void main(String[] args) {
-//         Case c1= new Case(0, 0, 2);
-//         Case c2= new Case(0, 3, 2);
-//         Case c3= new Case(2, 0, 2);
-//         Case c4= new Case();
-
-
-
-//         ArrayList<Case> l= new ArrayList<Case>();
-
-//         // l.add(c3);
-//         // l.add(c2);
-//         // l.add(c1);
-
-//         for(int i=0;i<16;i++){
-//             l.add(c4);
-//         }
-
-//         Board b= new Board(l);
-
-//         b.SetCaseAt(c1.GetX(),c1.GetY(), c1);
-//         b.SetCaseAt(c2.GetX(),c2.GetY(), c2);
-//         b.SetCaseAt(c3.GetX(),c3.GetY(), c3);
-
-//         System.out.println(b.toString());
-
-//         b.Spawn();
-
-//         System.out.println(b.toString());
-//     }
-// }
-
 public class Test{
     public static void main(String[] args){
         Case c1= new Case(0, 0, 2);
         Case c2= new Case(0, 3, 2);
         Case c3= new Case(2, 0, 2);
-        Case c4= new Case();
 
-        ArrayList<ArrayList<Case>> ct= new ArrayList<ArrayList<Case>>();
+        Board b= new Board();
 
-        for (int i=0;i<4;i++){
-            ArrayList<Case> c= new ArrayList<Case>();
-            for (int j=0;j<4;j++){
-                c.add(c4);
-            }
-            ct.add(c);
-        }
-
-        Board b= new Board(ct);
+        b.InitCases();
 
         b.SetCaseAt(c1.GetX(),c1.GetY(), c1);
         b.SetCaseAt(c2.GetX(),c2.GetY(), c2);
@@ -62,5 +14,8 @@ public class Test{
 
         b.Spawn();
         System.out.println(b.toString());
+        b.MoveTop();
+        System.out.println(b.toString());
+        //System.out.println(b.GetEmpty());
     }
 }
